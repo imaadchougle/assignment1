2c43ef0c228b4e7984209254b2b48b05426b05aa
imaadchougle <chougleimaad08@gmail.com>
2025-05-16 19:47:39 +0530

    Add files via upload

---
+++ pycourse.py
@@ -0,0 +1,30 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition=num1+num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "Undefined (cannot divide by zero)"
+
+
+
+
+
+print("addition",addition)
+print("subtraction",subtraction)
+print("multiplication",multiplication)
+print("division",division)
+
+
+a=input("enter your first name")
+b=input("enter your second name")
+full_name=a+" "+b
+print(f"\nHello, {full_name}! Welcome to the Python program.")
+
+
+
+
